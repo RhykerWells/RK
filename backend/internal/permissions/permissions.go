4b5f3d53c69d2b0b4c3b4eb5dfb03eb221cad61f
Rhyker W <a.rhykerw@gmail.com>
2026-07-16 06:22:53 +0100

    feat: update permission types to include human readable metadata

--- backend/internal/permissions/permissions.go
+++ backend/internal/permissions/permissions.go
@@ -27,10 +27,86 @@ const (
 	PermissionFileDelete Permission = "file.delete"
 )
 
+type Definition struct {
+	HumanReadableName string
+	Description       string
+}
+
+var Definitions = map[Permission]Definition{
+	// Portal permissions
+	PermissionPortalManage: {
+		HumanReadableName: "Manage Portal",
+		Description:       "Allows management of portal settings",
+	},
+	PermissionPortalManageRoles: {
+		HumanReadableName: "Manage Portal Roles",
+		Description:       "Allows management of portal roles",
+	},
+	PermissionPortalManageMembers: {
+		HumanReadableName: "Manage Portal Members",
+		Description:       "Allows management of portal members",
+	},
+	PermissionPortalManageMemberRoles: {
+		HumanReadableName: "Manage Member Roles",
+		Description:       "Allows management of member roles",
+	},
+
+	// Folder permissions
+	PermissionFolderView: {
+		HumanReadableName: "View Folder",
+		Description:       "Allows viewing of folder contents",
+	},
+	PermissionFolderCreate: {
+		HumanReadableName: "Create Folder",
+		Description:       "Allows creation of new folders",
+	},
+	PermissionFolderRename: {
+		HumanReadableName: "Rename Folder",
+		Description:       "Allows renaming of folders",
+	},
+	PermissionFolderDelete: {
+		HumanReadableName: "Delete Folder",
+		Description:       "Allows deletion of folders",
+	},
+
+	// Document permissions
+	PermissionDocumentView: {
+		HumanReadableName: "View Document",
+		Description:       "Allows viewing of documents",
+	},
+	PermissionDocumentCreate: {
+		HumanReadableName: "Create Document",
+		Description:       "Allows creation of new documents",
+	},
+	PermissionDocumentEdit: {
+		HumanReadableName: "Edit Document",
+		Description:       "Allows editing of documents",
+	},
+	PermissionDocumentDelete: {
+		HumanReadableName: "Delete Document",
+		Description:       "Allows deletion of documents",
+	},
+	PermissionDocumentShare: {
+		HumanReadableName: "Share Document",
+		Description:       "Allows sharing of documents",
+	},
+
+	// File permissions
+	PermissionFileUpload: {
+		HumanReadableName: "Upload File",
+		Description:       "Allows uploading of files",
+	},
+	PermissionFileDelete: {
+		HumanReadableName: "Delete File",
+		Description:       "Allows deletion of files",
+	},
+}
+
 var AllPermissions = []Permission{
 	PermissionPortalManage,
-	PermissionPortalManageMembers,
 	PermissionPortalManageRoles,
+	PermissionPortalManageMembers,
+	PermissionPortalManageMemberRoles,
 
 	PermissionFolderView,
 	PermissionFolderCreate,
